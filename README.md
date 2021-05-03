60f87d9f553284e6b8665bbd565a03a65e08714f
wilstaley <wilstaley@gmail.com>
2021-05-02 19:01:50 -0600

    Add dynamic readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-My name is Wilson and today is Saturday, May 1.
+### Hi there 👋
+
+##### My name is Wilson Staley and I am a Software Developer in Colorado Springs, CO ⛰.  It is currently Sunday, May 2 ⌚. In 2021 I have made 135 commits, 19 PRs, and contributed to 8 projects 👨‍💻. I am currently working at Compassion International 🏢 to help release children from poverty in Jesus' name!
--- index.js
+++ index.js
@@ -1,30 +1,59 @@
-// index.js
-const Mustache = require('mustache');
-const fs = require('fs');
-const MUSTACHE_MAIN_DIR = './main.mustache';
-/**
- * DATA is the object that contains all
- * the data to be provided to Mustache
- * Notice the "name" and "date" property.
- */
-let DATA = {
-  name: 'Wilson',
-  date: new Date().toLocaleDateString('en-US', {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-  }),
-};
-/**
- * A - We open 'main.mustache'
- * B - We ask Mustache to render our file with the data
- * C - We create a README.md file with the generated output
- */
-function generateReadMe() {
+require('dotenv').config();
+
+const Mustache = require("mustache");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+const MUSTACHE_MAIN_DIR = "./main.mustache";
+const GH_GRAPHQL_URL = "https://api.github.com/graphql";
+
+(async () => {
+  console.log("fetching data...");
+  const year = new Date().getFullYear();
+  const query = `
+    query {
+      user(login: "wilstaley") {
+        contributionsCollection(from: "${year}-01-01T00:00:00Z") {
+          totalCommitContributions,
+          totalPullRequestContributions,
+          totalRepositoriesWithContributedCommits
+        }
+      }
+    }
+  `;
+
+  const opts = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `bearer ${process.env.GH_ACCESS_TOKEN}`,
+    },
+    body: JSON.stringify({ query }),
+  };
+  const res = await fetch(GH_GRAPHQL_URL, opts);
+  const {
+    data: {
+      user: { contributionsCollection: ghStats },
+    },
+  } = await res.json();
+
+  console.log("Creating readme...");
+  const DATA = {
+    date: new Date().toLocaleDateString("en-US", {
+      weekday: "long",
+      month: "long",
+      day: "numeric",
+    }),
+    year,
+    commits: ghStats.totalCommitContributions,
+    prs: ghStats.totalPullRequestContributions,
+    repos: ghStats.totalRepositoriesWithContributedCommits,
+  };
+
   fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
-    fs.writeFileSync('README.md', output);
+    fs.writeFileSync("README.md", output);
   });
-}
-generateReadMe();
+})();
+
